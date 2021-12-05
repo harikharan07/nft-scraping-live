caa8c02e8e0eb71b1533e40240837b8cc2b235fc
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2021-12-05 04:32:37 +0000

    added code with beautiful soup, and found method to find table tag

--- scraper.py
+++ scraper.py
@@ -1,1 +1,17 @@
-print('hello')
+from bs4 import BeautifulSoup
+import requests
+nft_trending_url = 'https://coinmarketcap.com/nft/collections'
+
+response = requests.get(nft_trending_url)
+print('status', response.status_code)
+print('Output',response.text[:1000])
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+
+doc = BeautifulSoup(response.text,'html.parser')
+
+#find the division containing table
+table_row_div = doc.find_all('table',class_='cmc-table')
+kj= len(table_row_div)
+print(kj)
+
