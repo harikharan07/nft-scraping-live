c07d7e00ae71180248eda37305e1519682448ae0
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2021-12-05 05:09:03 +0000

    trying to get table content

--- scraper.py
+++ scraper.py
@@ -1,17 +1,24 @@
-from bs4 import BeautifulSoup
-import requests
-nft_trending_url = 'https://coinmarketcap.com/nft/collections'
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-response = requests.get(nft_trending_url)
-print('status', response.status_code)
-print('Output',response.text[:1000])
-with open('trending.html', 'w') as f:
-  f.write(response.text)
+nft_trending_url = 'https://coinmarketcap.com/nft/collecttions'
 
-doc = BeautifulSoup(response.text,'html.parser')
+def get_driver():
+ chrome_options = Options()
+ chrome_options.add_argument('--no-sandbox')
+ chrome_options.add_argument('--headless')
+ chrome_options.add_argument('--disable-dev-shm-usage')
+ driver = webdriver.Chrome(options=chrome_options)
+ return driver
 
-#find the division containing table
-table_row_div = doc.find_all('table',class_='cmc-table')
-kj= len(table_row_div)
-print(kj)
-
+if __name__ == "__main__" :
+ print('Creating driver')
+ driver = get_driver()
+ print('fetching the page')
+ driver.get(nft_trending_url)
+ print('Page title:', driver.title)
+ 
+ print('get the list div')
+ name_list_table_class='cmc-table'
+ name_divs = driver.find_elements_by_class_name(name_list_table_class)
+ print(f'found {len(name_divs)}table')
